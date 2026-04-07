cd4a96ce39cc291c6c697f1103a818a833fc1d96
nachitocaradura <158576641+nachitocaradura@users.noreply.github.com>
2026-04-07 13:34:34 +0200

    Primera subida

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfTEVS4EhcQxuj3S_zN-zxn_rnRRM2RE8",
+  authDomain: "nuestra-app-e19af.firebaseapp.com",
+  projectId: "nuestra-app-e19af",
+  storageBucket: "nuestra-app-e19af.firebasestorage.app",
+  messagingSenderId: "839568134740",
+  appId: "1:839568134740:web:bf76380b03b9e90d1d13c1"
+};
+
+const app = initializeApp(firebaseConfig);
+const db  = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { db, auth, provider };

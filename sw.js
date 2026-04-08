974fb10b759aef0bc615704f9b56a35db2c4f861
nachitocaradura <158576641+nachitocaradura@users.noreply.github.com>
2026-04-08 23:37:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Configuración (Usa la versión 8 aquí por compatibilidad del SDK en SW)
 firebase.initializeApp({
   apiKey: "AIzaSyCfTEVS4EhcQxuj3S_zN-zxn_rnRRM2RE8",
   projectId: "nuestra-app-e19af",
@@ -11,26 +10,14 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// Este evento se dispara cuando llega una notificación y la APP ESTÁ CERRADA
+// Este evento maneja las notificaciones cuando la app está en SEGUNDO PLANO
 messaging.onBackgroundMessage((payload) => {
-  console.log('Mensaje recibido en segundo plano:', payload);
-  
-  const notificationTitle = payload.notification.title || "Nuevo mensaje 💕";
+  console.log('Recibido mensaje en segundo plano: ', payload);
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.image || 'icono-app-192.png',
-    badge: 'icono-app-192.png',
-    vibrate: [200, 100, 200],
-    data: { url: '/chat.html' }
+    icon: '/icono-app-192.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
-});
-
-// Al hacer click en la notificación
-self.addEventListener('notificationclick', (event) => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow(event.notification.data.url)
-  );
 });
